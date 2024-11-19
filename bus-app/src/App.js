ff9f3c46c43bae56e990b3ca582e01ed42786ab7
Chrysr17 <chrisrasaro@gmail.com>
2024-11-19 11:20:32 -0500

    Commit1 - Proyecto Bus

---
+++ bus-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import BusTable from './BusTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gestión de Buses</h1>
+      <BusTable />
+    </div>
+  );
+}
+
+export default App;
+
